34863b6cdbebb6c67dfa5b25b8414dd08f1ca245
Vaztonyo <tony.dewet7@gmail.com>
2017-03-30 14:13:30 +0200

    removed buttons and modified code to have bg-color set automatically and clear

--- matching_days.js
+++ matching_days.js
@@ -1,33 +1,32 @@
 var day1 = document.querySelector('.date1');
 var day2 = document.querySelector('.date2');
-var myButton = document.querySelector('.myButton');
 var table = document.querySelectorAll('.days');
-var clearButton = document.querySelector('.clearButton')
 
 var getThatDay = function(date) {
     var whatDay = date.getDay();
     return whatDay;
 };
 
-myButton.addEventListener('click', function() {
+var matchDays = function() {
+  clearDays();
     var theDay1 = new Date(day1.value);
     var theDay2 = new Date(day2.value);
     if (getThatDay(theDay1) === getThatDay(theDay2)) {
         table[getThatDay(theDay1)].classList.add('match');
-    } else {
-        table[getThatDay(theDay1)].classList.add('day1');
-        table[getThatDay(theDay2)].classList.add('day2');
+    }
+    else {
+      table[getThatDay(theDay1)].classList.add('day1');
+      table[getThatDay(theDay2)].classList.add('day2');
     }
 
-    clearButton.addEventListener('click', function(){
-        table[getThatDay(theDay1)].classList.remove('match');
-        table[getThatDay(theDay1)].classList.remove('day1');
-        table[getThatDay(theDay2)].classList.remove('day2');
-    });
-});
+};
 
-// clearButton.addEventListener('click', function(){
-//     table[getThatDay(theDay1)].classList.remove('match');
-//     table[getThatDay(theDay1)].classList.remove('day1');
-//     table[getThatDay(theDay2)].classList.remove('day2');
-// });
+var clearDays = function(){
+  for (var i = 0; i < table.length; i++) {
+    table[i].classList.remove('match');
+    table[i].classList.remove('day1');
+    table[i].classList.remove('day2');
+  }
+};
+day1.addEventListener('change', matchDays);
+day2.addEventListener('change', matchDays);
